67c288c9a46e2b75839fed2bd0e27a6a41964d5f
NguyenHoangAnhSE173545 <anhnhse173545@fpt.edu.vn>
2024-10-16 22:47:28 +0700

    fix port

--- src/pages/Manager/finals/sales-staff-management.jsx
+++ src/pages/Manager/finals/sales-staff-management.jsx
@@ -43,7 +43,7 @@ import { toast } from "@/components/ui/use-toast"
 //   }
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: 'http://localhost:8080',
   timeout: 5000,
 })
 
